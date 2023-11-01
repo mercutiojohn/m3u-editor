00625930c32bf80c6908d31c90ad70eb59de7966
gao_ychen <gao_ychen@cvicse.com>
2023-11-01 13:31:31 +0800

    Crud

---
+++ src/views/Crud.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="crud">
+   </div>
+</template>
+
+<script>
+import SplitView from "@/components/common-split-view/SplitView.vue"
+import SplitViewItem from "@/components/common-split-view/SplitViewItem.vue"
+
+export default {
+  name: "About",
+  components: {
+    SplitView,
+    SplitViewItem
+  },
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  watch: {},
+  computed: {},
+  created() {},
+  mounted() {},
+  methods: {},
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
