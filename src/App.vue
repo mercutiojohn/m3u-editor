a81e145ac3071477fe8b37366c49ef5b837bd6b9
gao_ychen <gao_ychen@cvicse.com>
2023-11-01 13:32:05 +0800

    crud

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
      <nav>
        <router-link to="/">Home</router-link> |
+       <router-link to="/crud">Crud</router-link> |
        <router-link to="/about">About</router-link>
      </nav>
      <router-view class="content"/>
