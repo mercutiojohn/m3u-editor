1143d0ba4715c06e69b3f621459a0e82510a90af
Mercutio <mercutio_john@foxmail.com>
2023-12-04 11:25:24 +0800

    rearrange code

--- src/router/index.js
+++ src/router/index.js
@@ -11,49 +11,9 @@ const routes = [
     component: Home
   },
   {
-    path: '/panel',
-    name: 'Panel',
-    component: () => import('@/views/Panel.vue')
-  },
-  {
-    path: '/crud',
-    name: 'Crud',
-    component: () => import('@/views/Crud/index.vue')
-  },
-  {
-    path: '/grid',
-    name: 'Grid',
-    component: () => import('@/views/Grid.vue')
-  },
-  {
     path: '/generate-m3u',
     name: 'GenerateM3U',
     component: () => import('@/views/GenerateM3U/index.vue')
-  },
-  {
-    path: '/ical-to-epg',
-    name: 'iCalToEPG',
-    component: () => import('@/views/iCalToEPG/index.vue')
-  },
-  {
-    path: '/geojson-combine',
-    name: 'GeoJSONCombine',
-    component: () => import('@/views/GeoJSONCombine/index.vue')
-  },
-  {
-    path: '/edit-ical',
-    name: 'EditiCal',
-    component: () => import('@/views/EditiCal/index.vue')
-  },
-  {
-    path: '/data-modeling',
-    name: 'DataModeling',
-    component: () => import('@/views/DataModeling/index.vue')
-  },
-  {
-    path: '/map-download',
-    name: 'MapDownload',
-    component: () => import('@/views/MapDownload/index.vue')
   },
   {
     path: '/about',
